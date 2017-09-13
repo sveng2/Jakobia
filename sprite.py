1dfad4d9a903f3bdf293da4181e9e5f921a23f66
Jakob Stolberg <jakobstolberg@Jakobs-MacBook-Pro.local>
2017-09-13 19:53:23 +0200

    sprites changed

--- sprite.py
+++ sprite.py
@@ -2,19 +2,10 @@ class Sprite:
     form = ''
     x = 0
     y = 0
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def load_sprite(self, path):
         self.form = open(path).readlines()
-
-tree1 = Sprite(10,2)
-print(tree1.form)
-
-tree1.load_sprite('/Users/selinaveng/Documents/jakobiamedjakob/Jakobia/sprites/tree.txt')
-
-for line in tree1.form:
-    print(line)
-print(tree1.form)
