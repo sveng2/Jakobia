bb3d23bd564afdd4044759edcf40cb202270bab8
Selina Veng <selinaveng@Selinas-MacBook-Pro-2.local>
2017-09-13 19:41:57 +0200

    adding sprite.py with new method that gets form from sprites

--- sprite.py
+++ sprite.py
@@ -1,7 +1,20 @@
 class Sprite:
-
-    def __init__(self, x, y, height, width):
+    form = ''
+    x = 0
+    y = 0
+    
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.height = height
-        self.width = width
+
+    def load_sprite(self, path):
+        self.form = open(path).readlines()
+
+tree1 = Sprite(10,2)
+print(tree1.form)
+
+tree1.load_sprite('/Users/selinaveng/Documents/jakobiamedjakob/Jakobia/sprites/tree.txt')
+
+for line in tree1.form:
+    print(line)
+print(tree1.form)
